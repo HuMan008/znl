af06713963657002f0e9cac042d73d50b2b99e23
苏亚江 <yizhishita@126.com>
2017-04-21 10:07:19 +0800

    s

--- src/main/java/cn/gotoil/znl/web/controller/BaseController.java
+++ src/main/java/cn/gotoil/znl/web/controller/BaseController.java
@@ -1,7 +1,13 @@
 package cn.gotoil.znl.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by wh on 2017/4/21.
  */
+
+@RestController
+@RequestMapping("/pay")
 public class BaseController {
 }

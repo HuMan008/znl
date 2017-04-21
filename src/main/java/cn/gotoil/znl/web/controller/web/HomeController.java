e7b76900df45fa0175dd845716bb9dcafe3fda64
wanghong <1255160510@qq.com>
2017-04-21 11:00:30 +0800

    11

--- src/main/java/cn/gotoil/znl/web/controller/web/HomeController.java
+++ src/main/java/cn/gotoil/znl/web/controller/web/HomeController.java
@@ -13,24 +13,47 @@ import javax.servlet.http.HttpServletRequest;
  * Created by Suyj <suyajiang@gotoil.cn> on 2017/2/23.16:45
  */
 @Controller
-@RequestMapping("/web")
+//@RequestMapping("/")
 public class HomeController {
 
 
 
     @RequestMapping(value = "/",method=RequestMethod.GET)
     public String home(Model model,HttpServletRequest httpServletRequest) {
+
+        model.addAttribute("selectedItem", "index"  );
+
         return "index";
     }
-
 
     @RequestMapping(value = "/index",method=RequestMethod.GET)
+    public String index_(Model model,HttpServletRequest httpServletRequest) {
+
+        model.addAttribute("selectedItem", "index"  );
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/web/modifyPwd",method=RequestMethod.GET)
+    public String modifyPwd(Model model,HttpServletRequest httpServletRequest) {
+
+//        model.addAttribute("selectedItem", "index"  );
+
+        return "users/modpwd";
+    }
+
+
+    @RequestMapping(value = "/web/index",method=RequestMethod.GET)
     public String index(Model model,HttpServletRequest httpServletRequest) {
+
+        model.addAttribute("selectedItem", "index"  );
+
+
         return "index";
     }
 
 
-    @RequestMapping(value="/logout",method=RequestMethod.GET)
+    @RequestMapping(value="/web/logout",method=RequestMethod.GET)
     public @ResponseBody Object logout(RedirectAttributes redirectAttributes ){
         //使用权限管理工具进行用户的退出，跳出登录，给出提示信息
 

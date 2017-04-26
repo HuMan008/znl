904b6954852003e8ac2250435bd74eeb55217229
wanghong <1255160510@qq.com>
2017-04-26 10:30:51 +0800

    11

--- src/main/java/cn/gotoil/znl/config/define/AlipayConfig.java
+++ src/main/java/cn/gotoil/znl/config/define/AlipayConfig.java
@@ -55,9 +55,9 @@ public class AlipayConfig {
 
     //--------------------------------------------------------------------------------------------
     // 服务器异步通知页面路径 需http://或者https://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    public static String notify_url = "http://120.25.91.237:8086/alipay/notify_url";
+    public static String notify_url = "http://120.25.91.237:8086/alipay/v1/notify_url";
     // 页面跳转同步通知页面路径 需http://或者https://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问 商户可以自定义同步跳转地址
-    public static String return_url = "http://120.25.91.237:8086/alipay/return_url";
+    public static String return_url = "http://120.25.91.237:8086/alipay/v1/return_url";
     //--------------------------------------------------------------------------------------------
 
 }
